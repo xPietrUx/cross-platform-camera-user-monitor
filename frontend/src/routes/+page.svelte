1be2aa8f41be88b1d4374c0d029095225810d0ad
Piotr Dusiński <piotrdusinski01@gmail.com>
2025-11-10 17:48:39 +0100

    feature: main page init

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="cover">
+<h1>Przykład wyglądu naszego projektu</h1>
+<div class="card">
+    <p>oto przykład karty</p>
+</div>
+<p>Wszelkie zmienne są w ../styles/base/_variables</p>
+<p>Powodzenia :)</p>
+</div>
+
+<style>
+    /* style możemy nazywać w różnych plikach tak 
+    samo a dać inne wyglądy do póki piszem je w 
+    sekcji style w danym pliku .svlete, 
+    a nie np w example.css */
+    .cover {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    .card{
+        border: var(--border);
+        border-radius: var(--border-radius-sm);
+        padding: var(--spacing-md);
+    }
+
+</style>
+
