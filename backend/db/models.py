a5647d88a4c584def7dc66e1c83c77fc8109a815
Piotr Dusiński <piotrdusinski01@gmail.com>
2025-11-27 16:20:18 +0100

    feature: model of User

---
+++ backend/db/models.py
@@ -0,0 +1,11 @@
+from typing import Optional
+from sqlmodel import Field, SQLModel
+
+
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    surname: str
+    email: str
+    online_status: bool
+    password: str
