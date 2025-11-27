ff446e52cef2729f3ee4e2df0471c3c32b164412
Piotr Dusiński <piotrdusinski01@gmail.com>
2025-11-27 16:47:27 +0100

    fix: remove optional from id

--- backend/db/models.py
+++ backend/db/models.py
@@ -1,9 +1,8 @@
-from typing import Optional
 from sqlmodel import Field, SQLModel
 
 
 class User(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+    id: int = Field(default=None, primary_key=True)
     name: str
     surname: str
     email: str
