dc4317dba570f9b7aecdefc73de6a426fbb2a3f1
Piotr Dusiński <piotrdusinski01@gmail.com>
2025-12-31 11:21:42 +0100

    fix: przeniesienie autetynkacji

---
+++ backend/dependencies.py
@@ -0,0 +1,50 @@
+from fastapi import Depends, HTTPException, status, Query
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from sqlmodel import Session, select
+from db.database import get_session
+from db.models import User
+
+from utils.security import SECRET_KEY, ALGORITHM
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login", auto_error=False)
+
+
+def get_token_from_header_or_query(
+    header_token: str = Depends(oauth2_scheme),
+    query_token: str = Query(None, alias="token"),
+):
+    if header_token:
+        return header_token
+    if query_token:
+        return query_token
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Brak tokena uwierzytelniającego",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+
+async def get_current_user(
+    token: str = Depends(get_token_from_header_or_query),
+    session: Session = Depends(get_session),
+) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Nieprawidłowe dane uwierzytelniające",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        # Teraz używamy poprawnego klucza do weryfikacji
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user = session.exec(select(User).where(User.email == email)).first()
+    if user is None:
+        raise credentials_exception
+
+    return user
