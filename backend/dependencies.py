17d0a9137cfc3ba50662c413ba11574782a22962
Piotr Dusiński <piotrdusinski01@gmail.com>
2025-12-31 11:24:34 +0100

    feature: dodanie autetynkacji w dostępniejszym miejscu

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -35,7 +35,6 @@ async def get_current_user(
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        # Teraz używamy poprawnego klucza do weryfikacji
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         email: str = payload.get("sub")
         if email is None:
