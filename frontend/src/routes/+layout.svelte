1beb5ef2c92adc983e447e16494dbd1c0d655b52
Piotr Dusiński <piotrdusinski01@gmail.com>
2026-01-10 17:14:06 +0100

    layout update

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -396,28 +396,6 @@
                     {/if}
                 </a>
             </li>
-
-            <li>
-                <a href="/camera" class="menu-item" on:click={(e) => nav(e, '/camera')}>
-                    <svg
-                        class="menu-icon"
-                        width="20"
-                        height="20"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                    >
-                        <path
-                            d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"
-                        />
-                        <circle cx="12" cy="13" r="4" />
-                    </svg>
-                    {#if isMenuExpanded}
-                        <span>Podgląd kamery</span>
-                    {/if}
-                </a>
-            </li>
         </ul>
     </nav>
 
